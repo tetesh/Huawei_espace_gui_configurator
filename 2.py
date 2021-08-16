e3ad9d26c16c5a4c0312f03b90d17c13ed25cb35
tetesh <B30A11Lyfcnz>
2021-08-16 16:49:41 +0300

    add

--- 2.py
+++ 2.py
@@ -1,25 +1,111 @@
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
-import time
+from selenium.common.exceptions import NoSuchElementException
+import time, os, sys, threading
+from tkinter import *
+import tkinter.ttk as ttk
 
-chrome_options = Options()
-# chrome_options.add_argument("--headless")
+def all_func(ip_addr, number, password):
+    #открываем шаблон конфига и исправляем в нем учетные данные. Важно: учетные данные от веб морды уже исправлены в шаблоне на: admin:Huawei79, а также уже указан мой server asterisk, и мой ntp server. Вам же будет проще подменит Config_old на свой!
+    with open('Config-eSpace7910_old.xml', 'r') as f:
+        old_data = f.read()
+    new_data_1 = old_data.replace(
+        '<Account1 Enable="1" Account="" LogOut="0" LabelName="" UCAccount="" PGMNumber="" CorpID="" PhysicalLocation="" JointUserNum="">\n<Auth UserName="" Passwd=""',
+        f'<Account1 Enable="1" Account="{number}" LogOut="0" LabelName="{number}" UCAccount="" PGMNumber="" CorpID="" PhysicalLocation="" JointUserNum="">\n<Auth UserName="{number}" Passwd="{password}"')
+    new_data_2 = new_data_1.replace(
+        '<UIEMUser UserNO="" UserName="" PassWord="" UnLoadTime="" isLogOut="0" isStorePasswd="0" timeTag="0" ContactSyncTime="0" Roaming="0" RoamingTime="0"',
+        f'<UIEMUser UserNO="{number}" UserName="{number}" PassWord="{password}" UnLoadTime="" isLogOut="0" isStorePasswd="0" timeTag="0" ContactSyncTime="0" Roaming="0" RoamingTime="0"')
+    with open('Config-eSpace7910.xml', 'w') as f:
+        f.write(new_data_2)
 
-capabilities = DesiredCapabilities.CHROME.copy()
-capabilities['acceptSslCerts'] = True
-capabilities['acceptInsecureCerts'] = True
+    #Добавляем аргумет заставляющий работать Chrome в скрытном режиме
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
 
-# Получаем в переменную browser указатель на браузер
-browser = webdriver.Chrome(chrome_options=chrome_options,desired_capabilities=capabilities)
+    #Отключаем проверку сертификатов
+    capabilities = DesiredCapabilities.CHROME.copy()
+    capabilities['acceptSslCerts'] = True
+    capabilities['acceptInsecureCerts'] = True
 
-# Переходим на страницу, на которой нужно что-то сделать
-browser.get('http://demo.mt.lv/webfig')
-browser.get('http://demo.mt.lv/webfig/#Bridge')
-time.sleep(12)
-add_bridge = browser.find_element_by_class_name('button')
-add_bridge.click()
-rw_value = browser.find_element_by_xpath('//*[@id="content"]/table[3]/tbody[3]/tr/td[3]/input')
-test = 'bridgetest'
-rw_value.clear()
-rw_value.send_keys('test')
+    # Получаем в переменную browser указатель на webdriver
+    browser = webdriver.Chrome(chrome_options=chrome_options, desired_capabilities=capabilities)
+    # Переходим на страницу
+    browser.get('https://' + ip_addr)
+    time.sleep(8)
+    # Ищем элемент ввода и отправляем стандартные учетные данные
+    login = browser.find_element_by_id('user_name_txt')
+    login.send_keys('admin')
+    # Ищем элемент ввода и отправляем стандартные учетные данные
+    passwd = browser.find_element_by_id('password_txt')
+    passwd.send_keys('admin123')
+    # Нажимаем кнопку авторизации
+    login_btn = browser.find_element_by_id('login_btn')
+    login_btn.click()
+    time.sleep(12)
+
+    try:  # Проверка авторизации (если пароль не стандартный - скрипт не зайдет в и нтерфейс и не найдет нужный элемент)
+        close_def_passwd = browser.find_element_by_id('ontBtn_yes_a')
+        close_def_passwd.click()
+    except NoSuchElementException:
+        print('Телефон уже был настроен, для настройки через эту программу сбростье его до заводских настроек!')
+        browser.close()
+        sys.exit()
+
+    time.sleep(1)
+    go_advanced = browser.find_element_by_id('label_advanced')
+    go_advanced.click()
+    go_cfg_button = browser.find_element_by_id('config_btn')
+    go_cfg_button.click()
+    time.sleep(1)
+    #передаем путь к нашему готовому файлу конфигурации
+    input_files = browser.find_element_by_class_name('input_file')
+    input_files.send_keys(os.path.abspath('Config-eSpace7910.xml'))
+    #Импортием конфигурацию
+    btn_import = browser.find_element_by_id('label_import_config_btn')
+    btn_import.click()
+    time.sleep(2)
+    browser.close()
+    print('Готово! После перезагрузки телефон будет готов!')
+
+root = Tk()
+root.geometry('450x250+1000+300')
+root.maxsize(450, 250)
+root.minsize(450, 250)
+f = Frame(root)
+f.pack(pady=10)
+
+
+l_ip = Label(f, text='Ip адрес:', fg='#008080')
+l_ip.grid(row=0, column=0, padx=10, pady=10, sticky=W)
+e_ip = Entry(f)
+e_ip.grid(row=0, column=2, sticky=W+E, padx=30, pady=10)
+l_user = Label(f, text='Sip user:', fg='#008080')
+l_user.grid(row=1, column=0, padx=10, pady=10, sticky=W)
+l_password = Label(f, text='Sip password:', padx=10, pady=10, fg='#008080')
+l_password.grid(row=2, column=0)
+e_user = Entry(f)
+e_user.grid(row=1, column=2, sticky=W+E, padx=30, pady=10)
+e_password = Entry(f)
+e_password.grid(row=2, column=2, sticky=W+E, padx=30, pady=10)
+pb = ttk.Progressbar(f, mode='determinate')
+pb.grid(row=4, column=0, columnspan=3, pady=10, sticky=W+E)
+pb_status = Label(f, fg='#008080')
+pb_status.grid(row=5, column=2, padx=10, pady=5, sticky=W)
+
+# def progress():
+#     pb_status['text'] = 'В процессе!'
+#     time.sleep(60)
+#     pb_status['text'] = 'Готово!'
+
+# def functions(ip_addr, number, password):
+#     global pb_status
+#     pb_status['text'] = 'В процессе!'
+#     all_func(ip_addr, number, password)
+#     time.sleep(60)
+#     pb_status['text'] = 'Готово!'
+
+
+btn_configure = Button(f, text='Настроить', padx=10, pady=5, fg='#008080', bg='lightgray', command=lambda: all_func(e_ip.get(), e_user.get(), e_password.get())).grid(row=3, column=0)
+
+root.mainloop()
