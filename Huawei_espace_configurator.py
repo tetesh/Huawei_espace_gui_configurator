a155b4eca99da4de269af11859f86a7889922443
Leonov <leonoff.sanya@mail.ru>
2021-08-18 16:42:54 +0300

    add functions

--- Huawei_espace_configurator.py
+++ Huawei_espace_configurator.py
@@ -5,11 +5,12 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 import time, os, sys
 from tkinter import *
-import tkinter.ttk as ttk
 
-def all_func(ip_addr, number, password):
-    pb.start()
+
+def all_func(ip_addr, number, password, web_login, web_password, flag):
+
     pb_status['text'] = 'В процессе!'
+    pb_status.update()
     # открываем шаблон конфига и исправляем в нем учетные данные. Важно: учетные данные от веб морды уже исправлены в шаблоне на: admin:Huawei79, а также уже указан мой server asterisk, и мой ntp server. Вам же будет проще подменит Config_old на свой!
     with open('Config-eSpace7910_old.xml', 'r') as f:
         old_data = f.read()
@@ -43,36 +44,51 @@ def all_func(ip_addr, number, password):
     except WebDriverException:
         print('ip адрес не доступен!')
         browser.close()
-        pb.stop()
-        pb_status['text'] = 'ERROR: ip адрес не доступен!'
+        # pb.stop()
+        pb_status['text'] = 'ERROR: Узел недоступен!'
+        return
 
     # Ищем элемент ввода и отправляем стандартные учетные данные
     try:
         login = browser.find_element_by_id('user_name_txt')
-        login.send_keys('admin')
+        login.send_keys(web_login)
     except NoSuchElementException:
         print('Это не ip телефон Huawei 7910')
         browser.close()
-        pb.stop()
-        pb_status['text'] = 'ERROR: Это не ip телефон Huawei 7910!'
+        pb_status['text'] = 'ERROR: Это не ip телефон!'
+        return
 
     # Ищем элемент ввода и отправляем стандартные учетные данные
     passwd = browser.find_element_by_id('password_txt')
-    passwd.send_keys('admin123')
+    passwd.send_keys(web_password)
     # Нажимаем кнопку авторизации
     login_btn = browser.find_element_by_id('login_btn')
     login_btn.click()
     time.sleep(12)
-
-    try:  # Проверка авторизации (если пароль не стандартный - скрипт не зайдет в и нтерфейс и не найдет нужный элемент)
-        close_def_passwd = browser.find_element_by_id('ontBtn_yes_a')
-        close_def_passwd.click()
-    except NoSuchElementException:
-        print('Телефон уже был настроен, для настройки через эту программу сбростье его до заводских настроек!')
-        browser.close()
-        pb.stop()
-        pb_status['text'] = 'ERROR: Телефон уже был настроен!'
-
+    if flag == 1:
+        try:  # Проверка авторизации (если пароль не стандартный - скрипт не найдет нужный элемент)
+            close_def_passwd = browser.find_element_by_id('ontBtn_yes_a')
+            close_def_passwd.click()
+        except NoSuchElementException:
+            print('Телефон уже был настроен, для настройки через эту программу сбростье его до заводских настроек!')
+            browser.close()
+            # pb.stop()
+            pb_status['text'] = 'ERROR: Телефон не на заводских настройках!'
+    else:
+        print('reconfigure')
+    go_status = browser.find_element_by_id('label_status')
+    go_status.click()
+    go_information = browser.find_element_by_id('version_btn')
+    go_information.click()
+    time.sleep(1)
+    version = browser.find_element_by_id('soft_version_td').text
+    if 'eSpace7910' in version:
+        print('phone eSpace7910')
+    else:
+        print('no')
+        pb_status['text'] = 'ERROR: Это не 7910!'
+        return
+    print('test')
     time.sleep(1)
     go_advanced = browser.find_element_by_id('label_advanced')
     go_advanced.click()
@@ -88,29 +104,28 @@ def all_func(ip_addr, number, password):
     time.sleep(2)
     browser.close()
     print('Готово! После перезагрузки телефон будет готов!')
-    time.sleep(60)
-    pb.stop()
-    pb_status['text'] = 'Готово!'
+    pb_status['text'] = 'Готово! Телефон будет перезагружен!'
+
 
 root = Tk()
-root.geometry('380x280+1000+300')
-root.maxsize(380, 280)
-root.minsize(380, 280)
+root.geometry('400x240+1000+300')
+root.maxsize(400, 240)
+root.minsize(400, 240)
 f = Frame(root, bg='#FBCEB1')
 f.pack()
 
 
 l_ip = Label(f, text='Введите ip адрес:', fg='black', bg='#FBCEB1', font=('Courier New', 12, 'bold'), padx=10)
-l_ip.grid(row=0, column=0, pady=10, sticky=W)
+l_ip.grid(row=0, column=0, columnspan=2, pady=10, sticky=W)
 
 e_ip = Entry(f)
 e_ip.grid(row=0, column=2, sticky=W+E, padx=10, pady=20)
 
 l_user = Label(f, text='Введите sip user:', fg='black', bg='#FBCEB1', font=('Courier New', 12, 'bold'), padx=10)
-l_user.grid(row=1, column=0, pady=10, sticky=W)
+l_user.grid(row=1, column=0, columnspan=2, pady=10, sticky=W)
 
 l_password = Label(f, text='Введите sip password:', padx=10, pady=10, fg='black', bg='#FBCEB1', font=('Courier New', 12, 'bold'))
-l_password.grid(row=2, column=0, pady=10, sticky=W)
+l_password.grid(row=2, column=0, columnspan=2, pady=10, sticky=W)
 
 e_user = Entry(f)
 e_user.grid(row=1, column=2, sticky=W+E, padx=10, pady=10)
@@ -118,13 +133,13 @@ e_user.grid(row=1, column=2, sticky=W+E, padx=10, pady=10)
 e_password = Entry(f)
 e_password.grid(row=2, column=2, sticky=W+E, padx=10, pady=10)
 
-pb = ttk.Progressbar(f, orient='horizontal', mode='indeterminate', length=280)
-pb.grid(row=5, column=0, columnspan=3, pady=5, padx=30, sticky=W+E)
+# pb = ttk.Progressbar(f, orient='horizontal', mode='indeterminate', length=280)
+# pb.grid(row=5, column=0, columnspan=3, pady=5, padx=30, sticky=W+E)
 
-pb_status = Label(f, fg='black', bg='#FBCEB1', font=('Courier New', 11, 'bold'))
+pb_status = Label(f, fg='black', bg='#FBCEB1', font=('Courier New', 10, 'bold'))
 pb_status.grid(row=4, column=0, columnspan=3, pady=10, padx=10, sticky=W)
 
 
-btn_configure = Button(f, text='Настроить', pady=5, fg='#008080', bg='lightgray', command=lambda: all_func(e_ip.get(), e_user.get(), e_password.get())).grid(row=3, column=0, sticky=W, padx=30)
-
+btn_configure = Button(f, text='Настроить', pady=5, fg='#008080', bg='lightgray', command=lambda: all_func(e_ip.get(), e_user.get(), e_password.get(), 'admin', 'admin123', 1)).grid(row=3, column=0, sticky=W, padx=30)
+btn_reconfigure = Button(f, text='Перерегистрация', pady=5, fg='#008080', bg='lightgray', command=lambda: all_func(e_ip.get(), e_user.get(), e_password.get(), 'admin', 'Huawei79!', 0)).grid(row=3, column=1, sticky=W, padx=10)
 root.mainloop()
